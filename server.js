bbd2279aede54a19ff60eb6f708da3e9f97da8f2
Dennis Yang <sinned@gmail.com>
2013-01-17 13:46:55 -0800

    showing zach

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Dennis World!');
+  response.send('Hello Zach World!');
 });
 
 var port = process.env.PORT || 8888;
